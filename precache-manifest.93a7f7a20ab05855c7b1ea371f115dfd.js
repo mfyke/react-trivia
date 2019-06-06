fa7e49a64bf3248faaa9fa8058cd2bf337771341
matt fyke <mfyke@live.com>
2019-06-06 10:52:59 -0700

    Updates

---
+++ precache-manifest.93a7f7a20ab05855c7b1ea371f115dfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab2d7a6fcbf6fd49c7e41aca0c8d209",
+    "url": "/react-trivia/index.html"
+  },
+  {
+    "revision": "1a94e61c08d5989586bc",
+    "url": "/react-trivia/static/css/main.63d1f59d.chunk.css"
+  },
+  {
+    "revision": "a2e4a361a044aed59c41",
+    "url": "/react-trivia/static/js/2.ef6d6d74.chunk.js"
+  },
+  {
+    "revision": "1a94e61c08d5989586bc",
+    "url": "/react-trivia/static/js/main.8df48406.chunk.js"
+  },
+  {
+    "revision": "d7d639b6d6fb5a48ad56",
+    "url": "/react-trivia/static/js/runtime~main.46ce7e89.js"
+  }
+]);
